350010e6bd40a15581e3f21ad4be3b21ec126cf9
abaoxomtieu <htbqn2003@gmail.com>
2024-09-08 21:55:23 +0700

    [feat][abaoxomtieu]: apply pad and unpad method

--- src/inference/segment_inference.py
+++ src/inference/segment_inference.py
@@ -3,26 +3,16 @@ from src.utils.utils_segment import preprocess, postprocess
 from PIL import Image
 
 
-def inference(image_path, model_path,threshold=0.5):
+def inference(image_path, model_path,threshold=0.0):
     model = ort.InferenceSession(model_path)
     input = preprocess(image_path)
     outputs = postprocess(model.run(None, {"images": input}),threshold=threshold)
-    img = Image.open(image_path)
-    cropped_images = []
-    for obj in outputs:
-        x1, y1, x2, y2, label, prob= obj
-        cropped_img = img.crop((x1, y1, x2, y2))
-        cropped_img = {
-            "image": cropped_img,
-            "label": label,
-            "prob": prob,
-        }
-        cropped_images.append(cropped_img)
-    return cropped_images
+
+    return outputs
 
 
 
 if __name__ == "__main__":
     model_path = "../model/segment.onnx"
-    image_path = "./test.jpg"
+    image_path = "../../test.jpg"
     print(inference(image_path, model_path))
--- src/utils/utils_segment.py
+++ src/utils/utils_segment.py
@@ -1,6 +1,9 @@
 from PIL import Image
 import numpy as np
 import cv2
+from typing import Tuple
+import cv2
+import numpy as np
 
 class_names = [
     "Certifications",
@@ -17,19 +20,20 @@ class_names = [
 ]
 number_class_custom = int(len(class_names) + 4)
 img_width, img_height = None, None
+left = None
+top = None
+ratio = None
 
 
-def preprocess(image_path):
-    global img_width, img_height
-    img = Image.open(image_path)
-    img_width, img_height = img.size
-    img = img.convert("RGB")
-    img = img.resize((640, 640))
-    input = np.array(img)
-    input = input.transpose(2, 0, 1)
-    input = input.reshape(1, 3, 640, 640).astype("float32")
-    input = input / 255.0
-    return input
+def preprocess(img: np.array, shape=(640, 640)) -> np.array:
+    global img_width, img_height, left, top, ratio
+    img, ratio, (left, top) = resize_and_pad(img, new_shape=shape)
+    img_height, img_width, _ = img.shape
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.transpose(2, 0, 1)
+    img = img.reshape(1, 3, 640, 640).astype("float32")
+    img = img / 255.0
+    return img
 
 
 def extract_box(outputs):
@@ -121,8 +125,73 @@ def postprocess(outputs, threshold):
     # apply non-maximum suppression
     objects.sort(key=lambda x: x[5], reverse=True)
     result = []
-    while len(objects) > 0:
-        result.append(objects[0])
-        objects = [object for object in objects if iou(object, objects[0]) < 0.7]
+    while objects:
+        obj = objects.pop(0)
+        result.append(obj)
+        objects = [other_obj for other_obj in objects if iou(other_obj, obj) < 0.7]
+    del objects
 
-    return result
+    cropped_images = [
+        {
+            "box": unpad_and_resize_boxes(obj[:4], ratio, left, top),
+            "label": obj[4],
+            "prob": obj[5],
+        }
+        for obj in result
+    ]
+    return cropped_images
+
+
+def resize_and_pad(
+    image: np.array,
+    new_shape: Tuple[int, int],
+    padding_color: Tuple[int] = (144, 144, 144),
+) -> np.array:
+    h_org, w_org = image.shape[:2]
+    w_new, h_new = new_shape
+    padd_left, padd_right, padd_top, padd_bottom = 0, 0, 0, 0
+
+    # Padding left to right
+    if h_org >= w_org:
+        img_resize = cv2.resize(image, (int(w_org * h_new / h_org), h_new))
+        h, w = img_resize.shape[:2]
+        padd_left = (w_new - w) // 2
+        padd_right = w_new - w - padd_left
+        ratio = h_new / h_org
+
+    # Padding top to bottom
+    if h_org < w_org:
+        img_resize = cv2.resize(image, (w_new, int(h_org * w_new / w_org)))
+        h, w = img_resize.shape[:2]
+        padd_top = (h_new - h) // 2
+        padd_bottom = h_new - h - padd_top
+        ratio = w_new / w_org
+
+    image = cv2.copyMakeBorder(
+        img_resize,
+        padd_top,
+        padd_bottom,
+        padd_left,
+        padd_right,
+        cv2.BORDER_CONSTANT,
+        None,
+        value=padding_color,
+    )
+
+    return image, ratio, (padd_left, padd_top)
+
+
+def unpad_and_resize_boxes(boxes, ratio, left, top):
+
+    if len(boxes) == 0:
+        return boxes
+    boxes = np.array(boxes)
+    if boxes.ndim == 1:
+        boxes = boxes.reshape(-1, 4)
+    boxes[:, [0, 2]] -= left
+    boxes[:, [1, 3]] -= top
+    boxes[:, :4] /= ratio
+    if len(boxes) == 1:
+        return boxes.flatten().tolist()
+    else:
+        return boxes.tolist()
