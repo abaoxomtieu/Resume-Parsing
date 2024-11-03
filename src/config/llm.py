92589792ce73bb69715c2b556c0c56fb52be4e85
abaoxomtieu <htbqn2003@gmail.com>
2024-11-03 15:31:00 +0700

    Revert "[feat][abaoxomtieu]: langgraph code"
    This reverts commit 1ea954da89163d806ca4d37828f8fd929b68d82d.

--- src/config/llm.py
+++ src/config/llm.py
@@ -1,6 +1,5 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-llm = ChatGoogleGenerativeAI(
+from langchain_google_genai import GoogleGenerativeAI
+llm = GoogleGenerativeAI(
     model="gemini-1.5-flash",
     temperature=0,
 )
--- src/prompt/promt.py
+++ src/prompt/promt.py
@@ -1,25 +1,20 @@
 from langchain_core.prompts import ChatPromptTemplate
 
-format_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-Role: You are an expert at extracting key information about projects from the "experience" section of an OCR'd resume.
-
-Instruction:
+format_prompt = """
+#Role: You are an expert at correcting spelling errors from interviewee's resume information.
+#Instruction:
 You are provided with a dictionary containing information from the user's resume by an OCR model. It may have misspellings or wrong entries.
 Please correct the spelling of each field.
 Move the content of the fields to more appropriate fields if necessary.
 You must not fabricate information and create new information.
 
-Output must be in JSON format following the same structure as the input.
-""",
-        ),
-        ("human", "{user_input}"),
-    ]
-)
+You must return JSON containing the same format as the original format:
 
+#Input:
+My resume is as follows: {input}
+
+Output must be in plain text:
+"""
 
 prompt_experience = ChatPromptTemplate.from_messages(
     [
